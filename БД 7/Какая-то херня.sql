8e10d9f53d88db5c320704e1bf2c15cd89213cf9
Egor Korolkov <101881246+egorkorolkov@users.noreply.github.com>
2023-08-26 20:52:37 +0300

    123.sql

--- БД 7/Какая-то херня.sql
+++ БД 7/Какая-то херня.sql
@@ -1,18 +1,1 @@
-------------
-/*DENY SELECT ON PUBLISHER TO testorole
-GRANT SELECT ON PUBLISHER TO testo
-SELECT * FROM PUBLISHER
 
-REVOKE SELECT ON PUBLISHER TO testo
-
-DENY SELECT ON PUBLISHER TO testo
-GRANT SELECT ON PUBLISHER TO testorole
-
-EXECUTE AS USER = 'testo';
-REVERT
-
-создать пользователя, у которого роль с deny и роль с dbowner
-зайти этим пользователем, добиться того, чтобы право появилось
-*/
-
-------------
